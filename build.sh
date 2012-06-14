0e0e34c011b536d1ab07f3c2997d01644c0bbeae
Julien Ammous <schmurfy@gmail.com>
2012-06-14 18:47:29 +0200

    fixed scripts
    - also removed bash dependency

--- build.sh
+++ build.sh
@@ -7,20 +7,23 @@ LUAJIT_PREFIX="/usr/local/lua"
 
 BASEDIR="$(dirname $0)/../.."
 
-pushd deps/libstatgrab/
+CP=$(pwd)
+cd deps/libstatgrab/
 ./autogen.sh
 ./configure
 cd src && make
 mkdir -p ../../../statgrab/ext/
 cp -f libstatgrab/.libs/libstatgrab.so ../../../statgrab/ext/
-popd
+
+cd $CP
 
 
 # luasocket
 CMAKE_FOLDER="_build_$(uname)"
 mkdir -p deps/luasocket/$CMAKE_FOLDER
 
-pushd deps/luasocket/$CMAKE_FOLDER
+cd deps/luasocket/$CMAKE_FOLDER
 cmake .. -DLUA_LIBRARIES=${LUAJIT_PREFIX}/lib/lua/5.1/ -DLUA_INCLUDE_DIR=${LUAJIT_PREFIX}/include/luajit-2.0/ -DLUA_LIBRARY=${LUAJIT_PREFIX}/lib/ -DCMAKE_INSTALL_PREFIX=${BASEDIR}/_install -DCMAKE_BUILD_TYPE=Release
 make install
-popd
+
+cd $CP
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(dirname $0)
 
-export LUA_PATH="${BASEDIR}/deps/_install/lib/?.lua;/usr/local/lua/lib/lua/?.lua;?.lua"
-export LUA_CPATH="${BASEDIR}/deps/_install/?.so;/usr/local/lua/lib/lua/?.so;statgrab/?.so"
+export LUA_PATH="${BASEDIR}/deps/_install/lib/lua/?.lua;/usr/local/lua/lib/lua/?.lua;?.lua"
+export LUA_CPATH="${BASEDIR}/deps/_install/lib/lua/?.so;/usr/local/lua/lib/lua/?.so;statgrab/?.so"
 
 luajit sender.lua
